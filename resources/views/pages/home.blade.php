8232898d8a63d855d9269d5d8f89aa2af2bc67d8
Antonioangy <dumitruangy26@gmail.com>
2022-02-03 15:54:51 +0100

    esercizio funzionante

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.main-layout')
 @section('content')
-<div class="centro">
+    <div class="centro">
+
         @auth
             <h1>Ciao {{ Auth::user() -> name }}</h1>
-            <button href="{{ route('logout') }}">LOGOUT</button>
+            <button>
+                <a href="{{ route('logout') }}">LOGOUT</a>
+            </button>
+                
         @else
             <h2>Esegui il login oppure registrati</h2>
-        
+    
             <br><hr><br>
         @endauth
-        
+    
         <h2>Registrazione:</h2>
         <form action="{{ route('register') }}" method="POST">
             @method('POST')
             @csrf
-        
+    
             <label for="name">Name</label>
             <input type="text" name="name"><br>
             <label for="email">E-mail</label>
@@ -26,14 +30,14 @@
             <br>
             <input type="submit" value="REGISTER">
         </form>
-        
+    
         <br>
-        
+    
         <h2>Login</h2>
         <form action="{{ route('login') }}" method="POST">
             @method('POST')
             @csrf
-        
+    
             <label for="email">E-mail</label>
             <input type="text" name="email"><br>
             <label for="password">Password</label>
@@ -41,7 +45,6 @@
             <br>
             <input type="submit" value="LOGIN">
         </form>
-    
     </div>
 
 @endsection
